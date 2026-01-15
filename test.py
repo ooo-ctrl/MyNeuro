ef00ee2ec5b676c6686c187bc01032904a47dbbd
iolo123 <s8167642@live.vu.edu.au>
2026-01-15 19:12:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,13 @@
 Prameters needed:
 API_KEY=your_api_key_here
 BASE_URL=your_base_url_here
+MODEL=your_model_here
+
+Parameter optional:
 ORGANIZATION=your_organization_here
 PROJECT=your_project_here
 WEBHOOK_SECRET=your_webhook_secret_here
-MODEL=your_model_here
+
 """
 from Model.client import AIClient
 import os
@@ -23,17 +26,20 @@ def main():
 
     # set up the AI client parameters from environment variables
     try:
-        Api_Key = os.getenv("API_KEY")
-        Base_Url = os.getenv("BASE_URL")
-        Organization = os.getenv("ORGANIZATION")
-        Project = os.getenv("PROJECT")
-        Webhook_Secret = os.getenv("WEBHOOK_SECRET")
-        Model = os.getenv("MODEL")
+        # get environment variables
+        Api_Key = os.getenv("API_KEY", None)
+        Base_Url = os.getenv("BASE_URL", None)
+        Organization = os.getenv("ORGANIZATION", None)
+        Project = os.getenv("PROJECT", None)
+        Webhook_Secret = os.getenv("WEBHOOK_SECRET", None)
+        Model = os.getenv("MODEL", None)
 
+        # check required parameters
         assert Api_Key is not None, "API_KEY is not set in environment variables."
         assert Base_Url is not None, "BASE_URL is not set in environment variables."
         assert Model is not None, "MODEL is not set in environment variables."        
 
+        # initialize the AIClient
         model = AIClient(
             api_key=Api_Key, 
             base_url=Base_Url, 
@@ -43,9 +49,12 @@ def main():
             model=Model
             )
     except Exception as e:
+        # log the error and exit for error initializing AIClient
         print(f"Error initializing AIClient: {e}")
         exit(1)
     print("AIClient initialized successfully.")
+
+
 
 if __name__ == "__main__":
     main()
